5b7d303a65d98d6096f824ebcb6ca1ef12193590
david puckett <david@brightrain.com>
2021-04-28 15:20:18 -0600

    all the WHALES

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,16 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import whaleGIF from "../gifs/whales.gif"
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Adopt a whale today</h1>
+    <p>Whales dough, dey nice</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <img src={whaleGIF} alt="Picture of Whale from BBC America" />
+    </div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
